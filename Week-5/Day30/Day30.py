7f8730db33802d8528d069a91e0ba2277add631c
Thirumala Sai Harsha Inumarthi <117017118+saiharsha3377@users.noreply.github.com>
2024-03-06 23:28:30 +0530

    Add files via upload

---
+++ Week-5/Day30/Day30.py
@@ -0,0 +1,10 @@
+age = 21
+height = 5.17
+first_name = "Inumarthi"
+greeting = "Hello, " + first_name + "!"
+print(greeting)
+age_square = age ** 2
+print(age_square)
+weight = 56
+bmi = weight / (height * height)
+print(bmi)
