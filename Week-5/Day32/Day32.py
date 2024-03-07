5d92ff51e2b900e64c7071c05fd1f7be3d8f6658
Thirumala Sai Harsha Inumarthi <117017118+saiharsha3377@users.noreply.github.com>
2024-03-07 21:52:28 +0530

    Add files via upload

---
+++ Week-5/Day32/Day32.py
@@ -0,0 +1,19 @@
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You are a minor.")
+    
+elif age >= 18 and age <= 65:
+    print("You are an adult.")
+    
+else:
+    print("You are a senior citizen.")
+
+number = float(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
